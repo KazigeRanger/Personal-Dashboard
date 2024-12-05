74b7e56f83fc70c008029b504b5a791fcf38ccc8
Renger Bierenga <bierengarenger@gmail.com>
2024-12-05 22:24:45 +0100

    Created the quoteGetter.ts file. This file gets quotes through an API Ninja API.

---
+++ src/modules/TypeScript/Getters/quoteGetter.ts
@@ -0,0 +1,19 @@
+import QUOTEAPIRESPONSE from "../../../interfaces/quoteApiResponse";
+
+async function getQuote(category: string): Promise<QUOTEAPIRESPONSE> {
+    const apiKey = "DINwyGkOgjKTRPVMYDXDew==NhQn2RxfLgkRqr3G";
+    const apiSource = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
+
+    try{
+        const apiResponse = fetch(apiSource, {
+            method: "GET",
+            headers: {
+                "X-Api-Key": apiKey
+            }
+        });
+        const apiResponseJSON = (await apiResponse).json();
+        return apiResponseJSON;
+    } catch (err) {
+        throw err;
+    }
+}
