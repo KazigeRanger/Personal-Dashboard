1abd733abe79d013c2f2a6c847c4abe1c21160b9
Renger Bierenga <bierengarenger@gmail.com>
2024-12-07 17:15:44 +0100

    Created the weatherApiResponse.ts and weatherApiResponse.js files. These files contain a type interface for the JSON that the weather Api returns.

---
+++ src/interfaces/weatherApiResponse.ts
@@ -0,0 +1,14 @@
+export default WEATHERAPIRESPONSE;
+
+interface WEATHERAPIRESPONSE {
+    wind_speed: number,
+    wind_degrees: number,
+    temp: number,
+    humidity: number,
+    sunset: number,
+    min_temp: number,
+    cloud_pct: number,
+    feels_like: number,
+    sunrise: number,
+    max_temp: number
+}
