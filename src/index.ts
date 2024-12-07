67eecb5c4b1c2a50b61eb7d8311c963c1338ee7f
Renger Bierenga <bierengarenger@gmail.com>
2024-12-07 11:57:16 +0100

    Created the getGeocodedLocation.ts file and changed the names of the quoteGetter.ts and quoteGetter.js files to getQuote.ts and getQuote.js

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import updateClock from "./modules/TypeScript/Setters/updateClock.js";
-import getQuote from "./modules/TypeScript/Getters/quoteGetter.js";
+import getQuote from "./modules/TypeScript/Getters/getQuote.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-    // Get a quote to display
-    const subtitle = document.getElementById("page-subtitle");
-    if (subtitle === null) {
-        console.error("Failed to get subtitle element.");
-        return;
-    }
-    getQuote("happiness")
-    .then((resolvedResponse) => {
-        subtitle.textContent = `"${resolvedResponse[0].quote}" - ${resolvedResponse[0].author}`;
-    })
-    .catch(error => console.error("Error caught: ", error));
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     // Get a quote to display
+//     const subtitle = document.getElementById("page-subtitle");
+//     if (subtitle === null) {
+//         console.error("Failed to get subtitle element.");
+//         return;
+//     }
+//     getQuote("happiness")
+//     .then((resolvedResponse) => {
+//         subtitle.textContent = `"${resolvedResponse[0].quote}" - ${resolvedResponse[0].author}`;
+//     })
+//     .catch(error => console.error("Error caught: ", error));
+// });
 
 // Do some nice things when the window loads.
 window.addEventListener("load", function() {
